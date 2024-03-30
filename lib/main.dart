f5d99e79e966a35742e8e8ded5f1e176ddb774e3
Eric Alan Rodriguez Contreras <licalan.sistemas@gmail.com>
2024-03-30 12:22:53 -0600

    minimal code changes after app compilation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-
-import 'package:cinemapedia/config/router/app_router.dart';
-import 'package:cinemapedia/config/theme/app_theme.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
+
+import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:cinemapedia/config/router/app_router.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
